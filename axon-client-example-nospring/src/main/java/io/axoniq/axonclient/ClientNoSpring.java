7574f227107e6ff32c9938b7063e2bf3c05cebc9
Frans van Buul <frans.vanbuul@axoniq.io>
2017-08-15 08:54:15 +0200

    Fix in synchronization between master/slaves.

--- axon-client-example-nospring/src/main/java/io/axoniq/axonclient/ClientNoSpring.java
+++ axon-client-example-nospring/src/main/java/io/axoniq/axonclient/ClientNoSpring.java
@@ -20,22 +20,23 @@ public class ClientNoSpring {
 
     public static void main(String[] args) throws Exception {
         System.out.println(InetAddress.getLocalHost().getCanonicalHostName());
-        EventStoreConfiguration eventStoreConfiguration = EventStoreConfiguration.newBuilder("event-store-instance-1:8123")
+        EventStoreConfiguration eventStoreConfiguration = EventStoreConfiguration.newBuilder("eventstore.axoniq.io:8123")
                 .flowControl(0, 10000, 1000)
                 .token( "4e173955-f887-465a-a7d8-2fdbca7e4e37")
                 .connectionRetry(1000, 3)
-                //.ssl("resources/axoniq-public.crt")
+                .ssl("resources/axoniq-public.crt")
                 .build();
 
         Serializer serializer = new JacksonSerializer();
         AxoniqEventStore eventStore = new AxoniqEventStore( eventStoreConfiguration, serializer);
         try {
-            TrackingEventStream stream = eventStore.openStream(null);
+            // TrackingEventStream stream = eventStore.openStream(null);
+            TrackingEventStream stream = eventStore.openStream(new GlobalSequenceTrackingToken(10000000));
             int count = 20;
             while (stream.hasNextAvailable(5, TimeUnit.MINUTES) && count > 0) {
                 TrackedEventMessage<?> event = stream.nextAvailable();
                 GenericTrackedDomainEventMessage e = (GenericTrackedDomainEventMessage)event;
-                System.out.println(e.getAggregateIdentifier());
+                System.out.println(event.trackingToken() + "=>" +  e.getAggregateIdentifier());
                 count--;
             }
         } catch( Exception ex) {
